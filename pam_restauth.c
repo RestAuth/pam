d7ab1e4e61974ebd404c4cef7a0dd9c53998e977
Mihai <viper@restauth.net>
2014-02-08 17:16:53 +0100

    re-indent pam_restauth.c using 4 spaces

--- pam_restauth.c
+++ pam_restauth.c
@@ -39,111 +39,111 @@
 /* utility function */
 static char* url_escape(const char *str)
 {
-  // allocate 3 times the size of str
-  char *escaped = malloc(strlen(str)*3+1);
-  if (!escaped) return NULL;
-  char *escaped_str = escaped;
-  
-  while (*str) {
-    if ((*str >= 'a' && *str <= 'z') || (*str >= 'A' && *str <= 'Z') || 
-	(*str >= '0' && *str <= '9'))
-      *escaped_str = *str;
-    else if (*str == ' ')
-      *escaped_str = '+';
-    else {
-      *escaped_str = '%';
-      *(escaped_str+1) = ((*str/16) < 10) ? ('0' + *str/16) : ('A' - 10 + *str/16);
-      *(escaped_str+2) = ((*str%16) < 10) ? ('0' + *str%16) : ('A' - 10 + *str%16);
-      escaped_str += 2;
+    // allocate 3 times the size of str
+    char *escaped = malloc(strlen(str)*3+1);
+    if (!escaped) return NULL;
+    char *escaped_str = escaped;
+
+    while (*str) {
+        if ((*str >= 'a' && *str <= 'z') || (*str >= 'A' && *str <= 'Z') ||
+                (*str >= '0' && *str <= '9'))
+            *escaped_str = *str;
+        else if (*str == ' ')
+            *escaped_str = '+';
+        else {
+            *escaped_str = '%';
+            *(escaped_str+1) = ((*str/16) < 10) ? ('0' + *str/16) : ('A' - 10 + *str/16);
+            *(escaped_str+2) = ((*str%16) < 10) ? ('0' + *str%16) : ('A' - 10 + *str%16);
+            escaped_str += 2;
+        }
+
+        escaped_str++;
+        str++;
     }
-    
-    escaped_str++;
-    str++;
-  }
-  *escaped_str = '\0';
-  
-  return escaped;
+    *escaped_str = '\0';
+
+    return escaped;
 }
 
 /* RESTAuth request dispatcher */
 static int pam_restauth_check(
-		const char *base_url,
-		const char *service_user,
-		const char *service_password,
-		const char *group,
-		int validate_certificate,
-		const char *user,
+        const char *base_url,
+        const char *service_user,
+        const char *service_password,
+        const char *group,
+        int validate_certificate,
+        const char *user,
         const char *password) {
 
-  /* allocate structures */
-  CURL *session = curl_easy_init();
-  char *escaped_user = url_escape(user);
-  char *escaped_password = url_escape(password);
-  char *url = malloc(strlen(base_url)+strlen("/users/")+strlen(user)*3+1+1);
-  char *post_data = malloc(strlen("password=")+strlen(password)*3+1);
-  int ret = -1;
+    /* allocate structures */
+    CURL *session = curl_easy_init();
+    char *escaped_user = url_escape(user);
+    char *escaped_password = url_escape(password);
+    char *url = malloc(strlen(base_url)+strlen("/users/")+strlen(user)*3+1+1);
+    char *post_data = malloc(strlen("password=")+strlen(password)*3+1);
+    int ret = -1;
 
-  if (!session || !escaped_user || !escaped_password || !url || !post_data)
-    goto cleanup;
+    if (!session || !escaped_user || !escaped_password || !url || !post_data)
+        goto cleanup;
 
-  /* create URL: <base url>/users/<user>/ */
-  sprintf(url, "%s%susers/%s/", base_url,
-           *(base_url+strlen(base_url)-1) == '/' ? "":"/",
-           escaped_user);
+    /* create URL: <base url>/users/<user>/ */
+    sprintf(url, "%s%susers/%s/", base_url,
+            *(base_url+strlen(base_url)-1) == '/' ? "":"/",
+            escaped_user);
 
-  /* create POST data: password=<password> */
-  sprintf(post_data, "password=%s", escaped_password);
+    /* create POST data: password=<password> */
+    sprintf(post_data, "password=%s", escaped_password);
 
-  /* set up CURL request */
-  curl_easy_setopt(session, CURLOPT_NETRC, CURL_NETRC_IGNORED);
-  curl_easy_setopt(session, CURLOPT_NOSIGNAL, 1L);
-  
-  curl_easy_setopt(session, CURLOPT_USERNAME, service_user);
-  curl_easy_setopt(session, CURLOPT_PASSWORD, service_password);
+    /* set up CURL request */
+    curl_easy_setopt(session, CURLOPT_NETRC, CURL_NETRC_IGNORED);
+    curl_easy_setopt(session, CURLOPT_NOSIGNAL, 1L);
 
-  if (!validate_certificate)
-	  curl_easy_setopt(session, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_easy_setopt(session, CURLOPT_USERNAME, service_user);
+    curl_easy_setopt(session, CURLOPT_PASSWORD, service_password);
 
-  curl_easy_setopt(session, CURLOPT_POST, 1L);
-  curl_easy_setopt(session, CURLOPT_POSTFIELDS, post_data);
-  
-  curl_easy_setopt(session, CURLOPT_URL, url);
+    if (!validate_certificate)
+        curl_easy_setopt(session, CURLOPT_SSL_VERIFYPEER, 0);
 
-  /* perform request */
-  int curl_http_code, curl_status = curl_easy_perform(session);
-  curl_status += curl_easy_getinfo(session, CURLINFO_RESPONSE_CODE,
-                 &curl_http_code);
+    curl_easy_setopt(session, CURLOPT_POST, 1L);
+    curl_easy_setopt(session, CURLOPT_POSTFIELDS, post_data);
 
-  /* TODO group check */
-  if (group)
-	  syslog(LOG_AUTHPRIV|LOG_WARNING, __FILE__ ": plugin does not support restauth group check yet!");
+    curl_easy_setopt(session, CURLOPT_URL, url);
 
-  if (curl_status == CURLE_OK && curl_http_code >= 200 && curl_http_code < 300)
-    ret = 0; /* success */
-  else
-    ret = -1; /* failure */
+    /* perform request */
+    int curl_http_code, curl_status = curl_easy_perform(session);
+    curl_status += curl_easy_getinfo(session, CURLINFO_RESPONSE_CODE,
+                                     &curl_http_code);
+
+    /* TODO group check */
+    if (group)
+        syslog(LOG_AUTHPRIV|LOG_WARNING, __FILE__ ": plugin does not support restauth group check yet!");
+
+    if (curl_status == CURLE_OK && curl_http_code >= 200 && curl_http_code < 300)
+        ret = 0; /* success */
+    else
+        ret = -1; /* failure */
 
 cleanup:
-  if (session) curl_easy_cleanup(session);
-  if (escaped_user) free(escaped_user);
-  if (escaped_password) free(escaped_password);
-  if (url) free(url);
-  if (post_data) free(post_data);
-  
-  return ret;
+    if (session) curl_easy_cleanup(session);
+    if (escaped_user) free(escaped_user);
+    if (escaped_password) free(escaped_password);
+    if (url) free(url);
+    if (post_data) free(post_data);
+
+    return ret;
 }
 
 static const char *string_prefix_match(const char *s, const char *prefix) {
-	const char *res = strstr(s, prefix);
-	if (!res || res != s)
-		return res;
+    const char *res = strstr(s, prefix);
+    if (!res || res != s)
+        return res;
 
-	return s+strlen(prefix);
+    return s+strlen(prefix);
 }
 
 PAM_EXTERN int
 pam_sm_authenticate(pam_handle_t *pamh, int flags,
-    int argc, const char *argv[])
+                    int argc, const char *argv[])
 {
     const char *user;
     char *password;
@@ -157,35 +157,35 @@ pam_sm_authenticate(pam_handle_t *pamh, int flags,
 
     /* parse all parameters */
     {
-    	int i = 0;
-    	while (i < argc) {
-    		const char *val;
+        int i = 0;
+        while (i < argc) {
+            const char *val;
 
-    		if ((val = string_prefix_match(argv[i], "url=")) != NULL)
-    			url = val;
-    		else if ((val = string_prefix_match(argv[i], "service_user=")) != NULL)
-    			service_user = val;
-    		else if ((val = string_prefix_match(argv[i], "service_password=")) != NULL)
-    		    service_password = val;
-    		else if ((val = string_prefix_match(argv[i], "group=")) != NULL)
-    		    group = val;
-    		else if ((val = string_prefix_match(argv[i], "validate_certificate=")) != NULL)
-    		    validate_certificate = !!strcmp(val, "no"); /* no = 0, everything else = 1 */
+            if ((val = string_prefix_match(argv[i], "url=")) != NULL)
+                url = val;
+            else if ((val = string_prefix_match(argv[i], "service_user=")) != NULL)
+                service_user = val;
+            else if ((val = string_prefix_match(argv[i], "service_password=")) != NULL)
+                service_password = val;
+            else if ((val = string_prefix_match(argv[i], "group=")) != NULL)
+                group = val;
+            else if ((val = string_prefix_match(argv[i], "validate_certificate=")) != NULL)
+                validate_certificate = !!strcmp(val, "no"); /* no = 0, everything else = 1 */
 
-    		i++;
-    	}
+            i++;
+        }
     }
 
     /* complain about missing arguments, return error */
     if (!url || !(*url))
-    	syslog(LOG_AUTHPRIV|LOG_ERR, __FILE__": missing or empty required argument 'url'");
+        syslog(LOG_AUTHPRIV|LOG_ERR, __FILE__": missing or empty required argument 'url'");
     if (!service_user)
-    	syslog(LOG_AUTHPRIV|LOG_ERR, __FILE__": missing required argument 'service_user'");
+        syslog(LOG_AUTHPRIV|LOG_ERR, __FILE__": missing required argument 'service_user'");
     if (!service_password)
-    	syslog(LOG_AUTHPRIV|LOG_ERR, __FILE__": missing required argument 'service_password'");
+        syslog(LOG_AUTHPRIV|LOG_ERR, __FILE__": missing required argument 'service_password'");
 
     if (!url || !(*url) || !service_user || !service_password)
-    	return PAM_AUTHINFO_UNAVAIL;
+        return PAM_AUTHINFO_UNAVAIL;
 
     /* get user */
     if ((pam_err = pam_get_user(pamh, &user, NULL)) != PAM_SUCCESS)
@@ -203,13 +203,13 @@ pam_sm_authenticate(pam_handle_t *pamh, int flags,
 
     /* compare passwords */
     if (pam_restauth_check(url, service_user, service_password,
-    		group, validate_certificate, user, password)) {
-      /* wait a bit */
-      sleep(2);
-      pam_err = PAM_AUTH_ERR; // TODO AUTHINFO_UNAVAIL (on hardware failure)
+                           group, validate_certificate, user, password)) {
+        /* wait a bit */
+        sleep(2);
+        pam_err = PAM_AUTH_ERR; // TODO AUTHINFO_UNAVAIL (on hardware failure)
     }
     else {
-      pam_err = PAM_SUCCESS;
+        pam_err = PAM_SUCCESS;
     }
     
     return (pam_err);
@@ -217,7 +217,7 @@ pam_sm_authenticate(pam_handle_t *pamh, int flags,
 
 PAM_EXTERN int
 pam_sm_setcred(pam_handle_t *pamh, int flags,
-    int argc, const char *argv[])
+               int argc, const char *argv[])
 {
 
     return (PAM_SUCCESS);
@@ -225,7 +225,7 @@ pam_sm_setcred(pam_handle_t *pamh, int flags,
 
 PAM_EXTERN int
 pam_sm_acct_mgmt(pam_handle_t *pamh, int flags,
-    int argc, const char *argv[])
+                 int argc, const char *argv[])
 {
 
     return (PAM_SUCCESS);
@@ -233,7 +233,7 @@ pam_sm_acct_mgmt(pam_handle_t *pamh, int flags,
 
 PAM_EXTERN int
 pam_sm_open_session(pam_handle_t *pamh, int flags,
-    int argc, const char *argv[])
+                    int argc, const char *argv[])
 {
 
     return (PAM_SUCCESS);
@@ -241,7 +241,7 @@ pam_sm_open_session(pam_handle_t *pamh, int flags,
 
 PAM_EXTERN int
 pam_sm_close_session(pam_handle_t *pamh, int flags,
-    int argc, const char *argv[])
+                     int argc, const char *argv[])
 {
 
     return (PAM_SUCCESS);
@@ -249,10 +249,10 @@ pam_sm_close_session(pam_handle_t *pamh, int flags,
 
 PAM_EXTERN int
 pam_sm_chauthtok(pam_handle_t *pamh, int flags,
-    int argc, const char *argv[])
+                 int argc, const char *argv[])
 {
-  /* TODO implement in the future, maybe */
-  return (PAM_SERVICE_ERR);
+    /* TODO implement in the future, maybe */
+    return (PAM_SERVICE_ERR);
 }
 
 #ifdef PAM_MODULE_ENTRY
